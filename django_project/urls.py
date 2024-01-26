d0c165ced2b1c5f11399f69a0059778736783fde
SachetVijay <81145614+SachetVijay@users.noreply.github.com>
2024-01-26 18:36:19 +0530

    Update

--- django_project/urls.py
+++ django_project/urls.py
@@ -17,6 +17,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
+from django.conf import settings
+from django.conf.urls.static import static
 from users import views as user_views
 
 urlpatterns = [
@@ -27,4 +29,7 @@ urlpatterns = [
     path('register/', user_views.register, name='register'),
     path('profile/', user_views.profile, name='profile'),
 ]
- 
+
+if settings.DEBUG:
+    
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
